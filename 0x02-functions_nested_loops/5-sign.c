b8b562bf7ee91e4585f04c59b0240d28ba923009
GideonApp <owokojo@gmail.com>
2023-03-22 14:34:10 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - whether number is negative or positive or zero
+ * @n: number
+ * Return:  1 , 0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	else if (n < 0)
+		_putchar('-');
+		return (-1);
+}
