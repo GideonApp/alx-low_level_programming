f1f509f04081262c637e6912f329b102e4fbe8b8
GideonApp <owokojo@gmail.com>
2023-03-22 16:21:26 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * print_last_digit - prints last digit
- * @i: parameter
+ * @n: parameter
  * Return: always last digit
  */
 
-int print_last_digit(int i)
+int print_last_digit(int n)
 {
 	if (n < 0)
 		n = -n;
