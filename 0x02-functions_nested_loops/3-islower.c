980f68fa7da5e0862fc1f5fdd686df9dbe3c43fb
GideonApp <owokojo@gmail.com>
2023-03-22 13:55:31 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - if c lowercase 0 and 1 otherwise
+ * c - int
+ * Return: 1 and 0 depending on condition
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+i}
