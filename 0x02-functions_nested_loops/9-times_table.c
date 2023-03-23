e378df4e5a7129ac4f977e2168a869111e42aa39
GideonApp <owokojo@gmail.com>
2023-03-22 17:20:12 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main"
+/**
+ * times_table - prints atable of numbers
+ */
+void times_table(void)
+{
+	int col, n;
+	int row = 0;
+
+	while (row < 10)
+	{
+		row++;
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (col = 1; col < 10; col++)
+		{
+			n = (col * row);
+			if ((n / 10) > 0)
+			{
+				_putchar((n / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((n % 10) + '0');
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
