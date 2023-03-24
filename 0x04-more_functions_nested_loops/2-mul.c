6ed02241313eca4e49593a1d0395fb9e07c8aed8
GideonApp <owokojo@gmail.com>
2023-03-24 14:12:36 -0700

    multiple

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
  * mul -multiples two integers
  * @a: parameter
  * @b: parameter
- * Retun: result
+ * Return: always the result
  */
-
 int mul(int a, int b)
 {
-	result = a * b;
-	return (result);
-}	
+	return (a * b);
+}
