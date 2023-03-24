7e86375327fb4b71fffad0939fdca2cf5e4a6991
GideonApp <owokojo@gmail.com>
2023-03-24 16:18:29 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a funtion that prints a diagonal line
+ * @n: parameter describing the height
+ */
+
+void print_diagonal(int n)
+{
+	int i, m;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (m = 1; m <= i; m++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
