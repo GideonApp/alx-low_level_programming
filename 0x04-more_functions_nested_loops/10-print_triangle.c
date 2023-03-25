12a3548ce3843989f1b05fb98081f2ce22420c36
GideonApp <owokojo@gmail.com>
2023-03-24 18:00:23 -0700

    all

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - funtion that prints atriangle
+ * @size: parameter
+ */
+void print_triangle(int size)
+{
+	int i, n;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (n = i; n < size; n++)
+			{
+				_putchar('');
+			}
+			for (n = 1; n <= i; n++)
+			{
+				_putchar('#');
+			}
+			-putchar('\n');
+		}
+	}
+}
