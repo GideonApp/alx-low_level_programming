47b8f3387568a0d489709f627ae907cf48d74468
GideonApp <owokojo@gmail.com>
2023-03-27 11:51:13 -0700

    a charater plus an end line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,15 @@
 void puts2(char *str)
 {
 	int n;
+	int l = 0;
 
-	for (n = 0; n != 0; n++)
+	while (str[0] != '\0')
+	{
+		l++
+	}
+	for (n = 0; n < len; n += 2)
 	{
 		_putchar(str[n]);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
