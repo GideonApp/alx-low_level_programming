619483661834af980445ccf7b0c10f7e80942bf3
GideonApp <owokojo@gmail.com>
2023-03-27 09:05:03 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_strlen - a function that retuns the length of a string
+ * @s: string
+ * Return: Always length of a string (Successful)
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	return (j);
+}
