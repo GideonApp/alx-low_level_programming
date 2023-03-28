2c155010eeedecf2d62669dc7e084380553164c5
GideonApp <owokojo@gmail.com>
2023-03-28 13:04:25 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate a random password
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, sum, j;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			j = 2772 - sum - '0';
+			sum += j;
+			putchar(j + '0');
+			break;
+		}
+	}
+	return (0);
+}
