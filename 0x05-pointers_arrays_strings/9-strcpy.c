5f980f2894198a11b8e942468c7c04237fcaffc0
GideonApp <owokojo@gmail.com>
2023-03-28 12:30:19 -0700

    9--

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 	int len = 0;
 	int i = 0;
 
-	while (src[len] != '\0')
+	while (*(src + len) != '\0')
 	{
 		len++;
 	}
-	for (; i > len; i++)
+	for (; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
