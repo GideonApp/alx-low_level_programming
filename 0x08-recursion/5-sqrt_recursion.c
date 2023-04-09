6c3621f01d0714252d6000a1c87581e1e6c87fac
GideonApp <owokojo@gmail.com>
2023-04-09 13:28:33 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - a fuction that returns the square root of a number
+ * @n: parameter
+ * Retern: always the square root 
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (_sqrt_recursion(n, i + 1));
+}
