b51a32190b59f91a928072c267dc1dee752475fd
GideonApp <owokojo@gmail.com>
2023-04-11 12:00:30 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,6 +3,47 @@
 int helperfunc(char *s, int len);
 
 /**
+ * length - determines the length of a string
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int length(char *s)
+{
+	int len = 1;
+
+	if (*s)
+	{
+		len++;
+		len += length(s + 1);
+	}
+	return (len);
+}
+
+/**
+ * helperfunc - extends the function
+ * @s: string
+ * @len: length
+ * @i: begining parameter
+ * Return: 0 or 1
+ */
+
+int helperfunc(char *s, int i, int len)
+{
+	if (s[i] >= s[len])
+	{
+		return (1);
+	}
+	else if (s[i] != s[len])
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperfunc(s, i + 1, len - 1));
+	}
+}
+/**
  * is_palindrome - determines whether a string is a palindrome or not
  * @s: string
  * Return: 0 or 1
@@ -10,33 +51,14 @@ int helperfunc(char *s, int len);
 
 int is_palindrome(char *s)
 {
-	int len = 1;
+	int len = length(s);
 
-	if (*s)
-	{
-		len++;
-		len += is_palindrome(s + 1);
-	}
-	return (helperfunc(s, len));
-}
-
-/**
- * helperfunc - extends the function
- * @s: string
- * @len: length
- * Return: 0 or 1
- */
-
-int helperfunc(char *s, int len)
-{
-	if (len == 0 || len == 1)
+	if (len <= 1)
 	{
 		return (1);
 	}
-	if (s[0] == s[len - 1])
+	else
 	{
-		s[len - 1] = '\0';
-		return (helperfunc((s + 1), (len - 1)));
+		return (helperfunc(s, 0, len - 1));
 	}
-	return (0);
 }
