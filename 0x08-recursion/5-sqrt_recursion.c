863552f35705890c341357e8dc5a0687cffd82b9
GideonApp <owokojo@gmail.com>
2023-04-11 04:49:08 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
+int sqrtpart(int n, int i);
+
 /**
  * _sqrt_recursion - a fuction that returns the square root of a number
  * @n: parameter
- * Retern: always the square root 
+ * Return: always the square root
  */
 
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
+}
+
+/**
+ * sqrtpart - it does the actual square root
+ * @n: parameter
+ * @i: parameter
+ * Return: the square root
+ */
+
+int sqrtpart(int n, int i)
+{
 	if (i * i > n)
 	{
 		return (-1);
@@ -22,5 +33,4 @@ int _sqrt_recursion(int n)
 	{
 		return (i);
 	}
-	i = 1 + return (_sqrt_recursion(n));
-}
+	return(sqrtpart(n, i + 1);
