4fbb2fa4be0c6a1b318b5c64c6111f799340ba41
GideonApp <owokojo@gmail.com>
2023-04-11 08:27:46 -0700

    1007

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * is_palindrome - determines whether a string is a palindrome or not
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int len = 1;
+
+	if (*s)
+	{
+		len++;
+		len += is_palindrome(s + 1);
+	}
+	return (helperfunc(s, len));
+}
+
+/**
+ * helperfunc - extends the function
+ * @s: string
+ * @len: length
+ * Return: 0 or 1
+ */
+
+int helperfunc(char *s, int len)
+{
+	if (len == 0 || len == 1)
+	{
+		return (1);
+	}
+	if (s[0] == s[len - 1])
+	{
+		s[len - 1] = '\0';
+		return (helperfunc((s + 1), (len - 1)));
+	}
+	return (0);
+}
