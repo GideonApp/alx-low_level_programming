21e66ed990718b80894af10f0031403b02580a3e
GideonApp <owokojo@gmail.com>
2023-04-11 06:55:35 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - determine whether a numberis prime or not
+ * @n: parameter
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	int i = n - 1;
+
+	if (i <= 1)
+	{
+		return (0);
+	}
+	if (n <= 0)
+	{
+		return (0);
+	}
+	if (!(n % i == 0 && i > 0))
+	{
+		return (1);
+	}
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+	i--;
+	i -= is_prime_number(n);
+}
