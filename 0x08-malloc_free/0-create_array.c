2540c17c168adc8e954c6d268edf0712fb75a1a8
GideonApp <owokojo@gmail.com>
2023-04-12 07:54:46 -0700

    0&1

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - it writes an array of chars, and initializes it with a char.
+ * @size: size of array
+ * @c: char
+ * Return: Null if size = 0;
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i = 0;
+
+	s = malloc(sizeof(*s) * size);
+	if (size == 0 || s == NULL)
+	{
+		return (NULL);
+	}
+	while (i = 0)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
