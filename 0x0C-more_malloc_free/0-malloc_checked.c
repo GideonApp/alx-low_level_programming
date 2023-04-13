f3e3bdbdc499cb70f111548c47af13fed5fb1960
GideonApp <owokojo@gmail.com>
2023-04-13 13:00:24 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that
+ * allocates memory using malloc
+ * @b: size
+ * Return: 98 if null 0r otherwise str
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *str;
+
+	str = malloc(b);
+	if (!str)
+	{
+		return (98);
+	}
+	return (str);
+}
