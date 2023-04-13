b96bc2bdfffb18580ce759523a24105c0f65ab27
GideonApp <owokojo@gmail.com>
2023-04-13 15:23:33 -0700

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _realloc - function that reallocates a memory block
+ * @ptr: string
+ * @old_size: parameter
+ * @new_size: parameter
+ * Return: Null and string
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *str;
+	char *str1;
+	unsigned int i;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	str1 = ptr;
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+		{
+			str[i] = str1[i];
+		}
+	}
+	else
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			str[i] = str1[i];
+		}
+	}
+	free(ptr);
+	return (str);
+}
