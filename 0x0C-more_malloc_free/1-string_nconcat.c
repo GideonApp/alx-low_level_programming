a41427da75f2bb6a5b50c293ff391d2227d6fcf4
GideonApp <owokojo@gmail.com>
2023-04-13 14:02:06 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0;
 	unsigned int i = 0, j = 0;
 
-	while (*s1)
+	while (s1 && s1[len1])
 	{
 		len1++;
 	}
-	while (*s2)
+	while (s2 && s2[len2])
 	{
 		len2++;
 	}
