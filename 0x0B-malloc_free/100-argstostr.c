e0c3668604102dbe60a48bfc44a36d4e3dff6efe
GideonApp <owokojo@gmail.com>
2023-04-13 07:06:22 -0700

    10

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,9 +18,9 @@ char *argstostr(int ac, char **av)
 	}
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
-			k += 1;
+			k++;
 		}
 	}
 	k += ac;
