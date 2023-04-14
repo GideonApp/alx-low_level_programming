f3064500d35e28aab51a9776ad304697e1268a7d
GideonApp <owokojo@gmail.com>
2023-04-14 14:15:56 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "main.h"
 
 /**
  * main - a program that prints its name followed by a new line
@@ -9,7 +10,7 @@
  * Return: 0 success
  */
 
-int main(int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
