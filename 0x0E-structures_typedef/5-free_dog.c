693112f76430bb160215cc76a80bf4d26c02a2b6
GideonApp <owokojo@gmail.com>
2023-04-17 13:57:01 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: parameter
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != '\0')
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
